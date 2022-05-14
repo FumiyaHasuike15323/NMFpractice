ef1e5bb5a4bc4517b9fa3a673a015201bfae6589
y-shimada <yuto2sleepy@gmail.com>
2022-05-14 13:14:55 +0900

    追加　位相の取得と反映

--- NMF.m
+++ NMF.m
@@ -5,12 +5,15 @@ K = 128;
 numIterative = 500;
 
 F = DGTtool;
-[inputWave, Fs] = audioread("music_piano.wav");
-inputMat = abs(F(inputWave));
+[inputWave, Fs] = audioread("music_guitar.wav");
+spec = F(inputWave);
+angleSpec = angle(spec);
+inputMat = abs(spec);
+
 
 % [W, H] = EuNMF(inputMat, K, numIterative);
-[W, H] = KLNMF(inputMat, K, numIterative);
-% [W, H] = ISNMF(inputMat, K, numIterative);
+% [W, H] = KLNMF(inputMat, K, numIterative);
+[W, H] = ISNMF(inputMat, K, numIterative);
 
-outputWave = F.pinv(W * H);
+outputWave = F.pinv((W * H) .* exp(1i * angleSpec));
 audiowrite("test_out.wav", outputWave / max(abs(outputWave)), Fs);
